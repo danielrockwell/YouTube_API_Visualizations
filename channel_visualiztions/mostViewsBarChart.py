62acf22f0a1c8284902b8bf8c868d0a4e92f3882
Daniel Rockwell <danieljosephrockwell@gmail.com>
2019-06-30 01:02:35 -0500

    Added dual y-axes and color preferences

--- channel_visualiztions/mostViewsBarChart.py
+++ channel_visualiztions/mostViewsBarChart.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 CSV_PATH = "../channel_data/earthling_ed.csv"
-plotTitle = 'Earthling Ed Views and Comments Correlation'
+plotTitle = "Earthling Ed's Views and Likes Correlation"
 saveImagePath = './plot_images/Earthling_TopViewsBarChart.png'
 
 
@@ -21,19 +21,30 @@ dislikes = df['dislikecount']
 # plt.style.use('seaborn')
 plt.xkcd()
 
-# Preferences for Plot
-# colors = np.random.rand(COLORS)
 
-# Plotting data from CSV
-plt.bar(titles, viewCount)
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('Video Titles')
+ax1.set_ylabel('View Count', color=color, fontsize=11)
+ax1.bar(titles,viewCount,color=color)
+ax1.tick_params(axis='y', labelcolor=color, labelsize=9)
+ax1.tick_params(axis='x', labelsize=8)
 
 
-plt.title(plotTitle)
-plt.xticks(rotation=45, ha='right')
-plt.xlabel('View Count')
-plt.ylabel('Number of Likes')
+ax2=ax1.twinx()
 
-plt.tight_layout()
-# plt.savefig(saveImagePath, dpi = 400)
+color = 'tab:blue'
+ax2.set_ylabel('Like Count',color=color, fontsize=11)
+ax2.plot(titles,likes,color=color, marker='_',linewidth='0',linestyle='None',mew=3.9,markersize=30)
+ax2.tick_params(axis='y', labelcolor=color,labelsize=9)
+
+fig.patch.set_facecolor('#E0E0E0')
+plt.setp(ax1.get_xticklabels(), ha="right",rotation=45)
+
+plt.title(plotTitle,fontdict = {'fontsize' : 20})
+
+fig.tight_layout()
+plt.savefig(saveImagePath, dpi = 400)
 
 plt.show()
